23a83abbf8ecfdf93ef7115cd0d196fa3e11cebd
wassimbenghazi <wassimbenghazi@gmail.com>
2020-02-29 23:50:54 +0100

    adding firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,17 @@ import { LoginComponent } from "./login/login.component";
 import { AgmCoreModule } from "@agm/core";
 import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
 import { MatDialogModule } from "@angular/material";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
     FormsModule,
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
     ReactiveFormsModule,
     HttpModule,
     ComponentsModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyApeRIerynH4dcK_2XYnYsIzT16jrW03lQ",
+    authDomain: "smart-parking-nateg.firebaseapp.com",
+    databaseURL: "https://smart-parking-nateg.firebaseio.com",
+    projectId: "smart-parking-nateg",
+    storageBucket: "smart-parking-nateg.appspot.com",
+    messagingSenderId: "324042955335",
+    appId: "1:324042955335:web:f6084abcb91d4744db6f62",
+    measurementId: "G-GWM00FM5DN"
+  }
 };
